6b651844109c22b42aa0d08ad2abb373817a09fa
Yash Bobde <ymbobde99@gmail.com>
2021-09-09 23:10:07 +0530

    Add files via upload

---
+++ Day 3 Arrays.js
@@ -0,0 +1,40 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getSecondLargest(nums) {
+    nums.sort((a, b) => b - a);
+    const highest = nums[0];
+    for (let index = 1; index < nums.length ; index++) {
+        if (nums[index] !== highest) {
+            return nums[index];
+        }
+    }
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
